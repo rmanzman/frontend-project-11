d7d8617b98ddce85801fe0251192b4ce9d3a5de6
Roman Kraizman <roman.krayzman@gmail.com>
2023-06-01 21:30:44 +0300

    added parser logic for rss feeds

---
+++ src/parser.js
@@ -0,0 +1,29 @@
+const parse = (data) => {
+  const parser = new DOMParser();
+  const parsedData = parser.parseFromString(data, 'application/xml');
+
+  const parseError = parsedData.querySelector('parsererror');
+  if (parseError) throw new Error('parseError');
+
+  const channel = parsedData.querySelector('channel');
+  const title = channel.querySelector('title').textContent;
+  const description = channel.querySelector('description').textContent;
+  const feed = { title, description };
+
+  const items = Array.from(parsedData.querySelectorAll('item'));
+
+  const posts = items.map((item) => {
+    const postTitle = item.querySelector('title').textContent;
+    const postLink = item.querySelector('link').textContent;
+    const postDescription = item.querySelector('description').textContent;
+    return {
+      title: postTitle,
+      link: postLink,
+      description: postDescription,
+    };
+  });
+
+  return { feed, posts };
+};
+
+export default parse;
