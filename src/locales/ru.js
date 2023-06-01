0e276a5d7370270e28f15487989def7d4cd1dc41
Roman Kraizman <roman.krayzman@gmail.com>
2023-06-01 21:31:31 +0300

    added locales for i18next

---
+++ src/locales/ru.js
@@ -0,0 +1,19 @@
+export default {
+  translation: {
+    errors: {
+      invalidURL: 'Ссылка должна быть валидным URL',
+      invalidRSS: 'Ресурс не содержит валидный RSS',
+      alreadyExistsRSS: 'RSS уже существует',
+      shouldNotBeBlank: 'Не должно быть пустым',
+      networkError: 'Ошибка сети',
+      defaultError: 'Что-то пошло не так...',
+    },
+    status: {
+      sending: 'RSS загружается',
+      success: 'RSS успешно загружен',
+    },
+    preview: 'Просмотр',
+    feeds: 'Фиды',
+    posts: 'Посты',
+  },
+};
